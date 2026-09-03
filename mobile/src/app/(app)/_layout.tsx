e3e8f404c90da1d921d27abbe88b4a120c64c6a2
Sahil Tyagi <sahiltyagi194@gmail.com>
2026-09-03 13:53:28 +0530

    configured the environment

--- mobile/src/app/(app)/_layout.tsx
+++ mobile/src/app/(app)/_layout.tsx
@@ -1,5 +1,4 @@
-import { DarkTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
+import { DarkTheme, Stack, ThemeProvider } from 'expo-router';
 import React from 'react';
 
 export default function AppLayout() {
